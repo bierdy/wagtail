40b99f2464e6afbf872d963d3832dd482fc652ee
bierdy <bierdy@yandex.ru>
2022-01-18 16:08:34 +0300

    Fix Wagtail filter config: our filters must start first.

--- src/Config/Filters.php
+++ src/Config/Filters.php
@@ -1,9 +1,7 @@
 <?php
 
-use Wagtail\Filters\Wagtail;
-
 $filters_config = config('Filters');
 
-$filters_config->aliases['wagtail'] = Wagtail::class;
-$filters_config->globals['before'][] = 'wagtail';
-$filters_config->globals['after'][] = 'wagtail';
+$filters_config->aliases['wagtail'] = Wagtail\Filters\Wagtail::class;
+array_unshift($filters_config->globals['before'], 'wagtail');
+array_unshift($filters_config->globals['after'], 'wagtail');
