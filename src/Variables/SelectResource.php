b5609cb51b42659ed7fc220cf3c358a74d075255
bierdy <bierdy@yandex.ru>
2022-01-19 14:26:57 +0300

    We do not need to duplicate init() method. Let's extend from Wagtail\Variables\Text.

--- src/Variables/SelectResource.php
+++ src/Variables/SelectResource.php
@@ -2,17 +2,7 @@
 
 namespace Wagtail\Variables;
 
-class SelectResource extends Variable implements VariableInterface
+class SelectResource extends Text implements VariableInterface
 {
-    public function init()
-    {
-        if (empty($this->post[$this->variable->name]) && ! empty($this->variable->value))
-            $this->variableValueModel->delete($this->variable->value->id);
-        
-        elseif (! empty($this->post[$this->variable->name]) && ! empty($this->variable->value))
-            $this->variableValueModel->update($this->variable->value->id, ['value' => $this->post[$this->variable->name]]);
-        
-        elseif (! empty($this->post[$this->variable->name]) && empty($this->variable->value))
-            $this->variableValueModel->insert(['resource_id' => $this->resource->id, 'variable_id' => $this->variable->id, 'value' => $this->post[$this->variable->name], 'order' => 0]);
-    }
+
 }
