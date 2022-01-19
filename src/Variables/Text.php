86eb145f2071a1d48d7f1e09517a650d13a4bddd
bierdy <bierdy@yandex.ru>
2022-01-19 16:18:03 +0300

    Fix Wagtail\Variables\Text::init: if post variable value exist use it any case.

--- src/Variables/Text.php
+++ src/Variables/Text.php
@@ -6,13 +6,13 @@ class Text extends Variable implements VariableInterface
 {
     public function init()
     {
-        if (empty($this->post[$this->variable->name]) && ! empty($this->variable->value))
+        if (! isset($this->post[$this->variable->name]) && ! empty($this->variable->value))
             $this->variableValueModel->delete($this->variable->value->id);
         
-        elseif (! empty($this->post[$this->variable->name]) && ! empty($this->variable->value))
+        elseif (isset($this->post[$this->variable->name]) && ! empty($this->variable->value))
             $this->variableValueModel->update($this->variable->value->id, ['value' => $this->post[$this->variable->name]]);
         
-        elseif (! empty($this->post[$this->variable->name]) && empty($this->variable->value))
+        elseif (isset($this->post[$this->variable->name]) && empty($this->variable->value))
             $this->variableValueModel->insert(['resource_id' => $this->resource->id, 'variable_id' => $this->variable->id, 'value' => $this->post[$this->variable->name], 'order' => 0]);
     }
 }
