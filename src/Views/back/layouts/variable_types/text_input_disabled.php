d84e642bd931a54849c86e2f22616f870b67807f
bierdy <bierdy@yandex.ru>
2022-01-21 09:37:24 +0300

    Change disabled input variable type to readonly input variable type.

--- src/Views/back/layouts/variable_types/text_input_disabled.php
+++
@@ -1,4 +0,0 @@
-<div class="mb-3">
-    <?= form_label($variable->title, $variable->name, ['class' => 'form-label']); ?>
-    <?= form_input($variable->name, $post[$variable->name] ?? $variable->value->value ?? '', ['class' => 'form-control', 'id' => $variable->name, 'disabled' => 'disabled']); ?>
-</div>
